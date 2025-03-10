08cd79d83c1e7726e380485201245fbd14fdc9b4
mike <burkovmu@gmail.com>
2025-03-10 17:09:38 +0500

    Fix: Remove unused AnimatePresence import from Pricing component

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { useState } from 'react'
 import { FaMobileAlt, FaTabletAlt, FaLaptop } from 'react-icons/fa'
 import OrderModal from './OrderModal'
